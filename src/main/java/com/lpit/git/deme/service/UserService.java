98e7a36cc157bcb87c7adeb222f6432a57a0b3ce
root <3317402143@qq.com>
2023-05-09 14:45:00 +0800

    second commit

--- src/main/java/com/lpit/git/deme/service/UserService.java
+++ src/main/java/com/lpit/git/deme/service/UserService.java
@@ -3,5 +3,6 @@ package com.lpit.git.deme.service;
 public class UserService {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word1");
     }
 }
